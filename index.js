84005161cf48479d2bb6dc5967fbb6579aa3305c
mckaymatt3 <mckaymatt@outlook.com>
2022-02-01 09:10:54 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(emphasize = '*'){
+    return function (adjective ='special') {
+        return `You are ${emphasize}${adjective}${emphasize}!`}
+}
